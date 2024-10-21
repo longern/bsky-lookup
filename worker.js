94d4129eaf5e1a3fd2406ad2ab15aa66e2056376
Siyu Long <i@longern.com>
2024-10-21 13:11:20 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,42 @@
+const HTML_TEMPLATE = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body {
+        font-family: sans-serif;
+        margin: 0;
+      }
+      main {
+        max-width: 600px;
+        margin: 0 auto;
+      }
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 8px 0;
+      }
+      li { display: flex; }
+      li>a {
+        padding: 8px 16px;
+        flex-grow: 1;
+        transition: background-color 0.3s;
+      }
+      li>a:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+      }
+    </style>
+  </head>
+  <body>
+    <main>
+      <ul>{{links}}</ul>
+    </main>
+  </body>
+</html>
+`;
+
 export default {
   async fetch(request, env) {
     const host = new URL(request.url).host;
@@ -7,19 +46,23 @@ export default {
     if (!zoneID || !env.CLOUDFLARE_API_KEY)
       return new Response("Bad Request", { status: 400 });
 
-    const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records?type=TXT&name.startswith=_atproto`
+    const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records?type=TXT&name.startswith=_atproto`;
     const response = await fetch(apiUrl, {
       headers: {
-        'Authorization': `Bearer ${env.CLOUDFLARE_API_KEY}`, 'Content-Type': 'application/json'
-      }
+        Authorization: `Bearer ${env.CLOUDFLARE_API_KEY}`,
+        "Content-Type": "application/json",
+      },
     });
     if (!response.ok) return response;
     const data = await response.json();
     const { result: records } = data;
-    const links = records.map(record => {
+    const links = records.map((record) => {
       const subdomain = record.name.replace("_atproto.", "");
-      return `<a href="https://${subdomain}">${subdomain}</a>`;
-    })
-    return new Response(links.join("<br/>"), { headers: { 'Content-Type': "text-html" } });
+      return `<li><a href="https://${subdomain}">@${subdomain}</a></li>`;
+    });
+    return new Response(
+      HTML_TEMPLATE.replace("{{links}}", links.join("")),
+      { headers: { "Content-Type": "text-html" } }
+    );
   },
 };
